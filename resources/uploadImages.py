4c553fc8044869eafb8973ac0e70a9671c30db47
tates2 <tates.2@osu.edu>
2022-10-08 15:12:34 -0400

    stuff

--- resources/uploadImages.py
+++ resources/uploadImages.py
@@ -3,14 +3,14 @@ import boto3
 s3 = boto3.resource('s3')
 
 # Get list of objects for indexing
-images=[('../photos/databasePhotos/A.jpg','A'),
-      ('../photos/databasePhotos/B.jpg','A'),
-      ('../photos/databasePhotos/C.jpg','C'),
+images=[('./photos/databasePhotos/A.jpg','A.jpg'),
+      ('./photos/databasePhotos/B.jpg','B.jpg'),
+      ('./photos/databasePhotos/C.jpg','C.jpg'),
       ]
 
 # Iterate through list to upload objects to S3   
 for image in images:
     file = open(image[0],'rb')
-    object = s3.Object('famouspersons-images','index/'+ image[0])
+    object = s3.Object('signlanguage-images','index/'+ image[1])
     ret = object.put(Body=file,
                     Metadata={'FullName':image[1]})
