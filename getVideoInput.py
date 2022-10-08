f1783e759354998721dcb39fdac1f3c5bf419d8a
Andrew Kolarits <kolarits.1@buckeyemail.osu.edu>
2022-10-08 10:19:55 -0400

    Got picture saving

--- getVideoInput.py
+++ getVideoInput.py
@@ -1,26 +1,34 @@
-# import the opencv library
-import cv2
-# TRY using an OCR to get text from image
-  
-# define a video capture object
-vid = cv2.VideoCapture(0)
-  
-while(True):
-      
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-      
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+import cv2 
+
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+while True:
+    check, frame = webcam.read()
+    print(check) #prints true as long as the webcam is running
+    print(frame) #prints matrix values of each framecd 
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+    if key == ord('s'): 
+        cv2.imwrite(filename='saved_img.jpg', img=frame)
+        webcam.release()
+        img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+        img_new = cv2.imshow("Captured Image", img_new)
+        cv2.waitKey(1650)
+        cv2.destroyAllWindows()
+        print("Processing image...")
+        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+        print("Converting RGB image to grayscale...")
+        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+        print("Converted RGB image to grayscale...")
+        print("Resizing image to 28x28 scale...")
+        img_ = cv2.resize(gray,(28,28))
+        print("Resized...")
+        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+        print("Image saved!")
+    elif key == ord('q'):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
         break
-  
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
--- handDectection.py
+++ handDectection.py
@@ -7,7 +7,7 @@ min_YCrCb = np.array([0,133,77],np.uint8)
 max_YCrCb = np.array([235,173,127],np.uint8)
 
 # Get pointer to video frames from primary device
-image = cv2.imread("/assets/img/skin-detection/rock.jpg") #Replace with image from web cam
+image = cv2.imread("/saved_img.jpg") #Replace with image from web cam
 imageYCrCb = cv2.cvtColor(image,cv2.COLOR_BGR2YCR_CB)
 skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
 
