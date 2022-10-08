3fc88e65b4e8f337d77be9091b725d45a6d10e1a
Andrew Kolarits <kolarits.1@buckeyemail.osu.edu>
2022-10-08 10:20:51 -0400

    Fixed name

--- handDectection.py
+++ handDectection.py
@@ -7,10 +7,10 @@ min_YCrCb = np.array([0,133,77],np.uint8)
 max_YCrCb = np.array([235,173,127],np.uint8)
 
 # Get pointer to video frames from primary device
-image = cv2.imread("/saved_img.jpg") #Replace with image from web cam
+image = cv2.imread("saved_img.jpg") #Replace with image from web cam
 imageYCrCb = cv2.cvtColor(image,cv2.COLOR_BGR2YCR_CB)
 skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
 
 skinYCrCb = cv2.bitwise_and(image, image, mask = skinRegionYCrCb)
 
-cv2.imwrite("/assets/img/skin-detection/ycrcb.png", np.hstack([image,skinYCrCb]))
+cv2.imwrite("saved_img.jpg", np.hstack([image,skinYCrCb]))
