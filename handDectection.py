67329b68ecd10f79648c4833304387c1c9bff345
tates2 <tates.2@osu.edu>
2022-10-08 10:11:56 -0400

    Hand Detection Outline

---
+++ handDectection.py
@@ -0,0 +1,16 @@
+# Required modules
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+min_YCrCb = np.array([0,133,77],np.uint8)
+max_YCrCb = np.array([235,173,127],np.uint8)
+
+# Get pointer to video frames from primary device
+image = cv2.imread("/assets/img/skin-detection/rock.jpg") #Replace with image from web cam
+imageYCrCb = cv2.cvtColor(image,cv2.COLOR_BGR2YCR_CB)
+skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+
+skinYCrCb = cv2.bitwise_and(image, image, mask = skinRegionYCrCb)
+
+cv2.imwrite("/assets/img/skin-detection/ycrcb.png", np.hstack([image,skinYCrCb]))
