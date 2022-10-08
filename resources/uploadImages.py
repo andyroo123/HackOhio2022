43bbb3c14e498e1731a2b359b482ae89c7072eae
Andrew Kolarits <kolarits.1@buckeyemail.osu.edu>
2022-10-08 16:22:43 -0400

    Restructured database

--- resources/uploadImages.py
+++ resources/uploadImages.py
@@ -3,14 +3,14 @@ import boto3
 s3 = boto3.resource('s3')
 
 # Get list of objects for indexing
-images=[('../photos/databasePhotos/A.jpg','A'),
-      ('../photos/databasePhotos/B.jpg','A'),
-      ('../photos/databasePhotos/C.jpg','C'),
+images=[('./photos/databasePhotos/A.jpg','A'),
+      ('./photos/databasePhotos/B.jpg','B'),
+      ('./photos/databasePhotos/C.jpg','C'),
       ]
 
 # Iterate through list to upload objects to S3   
 for image in images:
     file = open(image[0],'rb')
-    object = s3.Object('famouspersons-images','index/'+ image[0])
+    object = s3.Object('famouspersons-images2','index/'+ image[0])
     ret = object.put(Body=file,
                     Metadata={'FullName':image[1]})
