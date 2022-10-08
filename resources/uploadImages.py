a9363b86ac554a87f1b0e875e5b1f61ad27d8359
tates2 <tates.2@osu.edu>
2022-10-08 16:03:21 -0400

    Added rekognition file to detect image recognition with made database

--- resources/uploadImages.py
+++ resources/uploadImages.py
@@ -8,9 +8,9 @@ images=[('./photos/databasePhotos/A.jpg','A.jpg'),
       ('./photos/databasePhotos/C.jpg','C.jpg'),
       ]
 
-# Iterate through list to upload objects to S3   
+# Iterate through list to upload objects to S3
 for image in images:
     file = open(image[0],'rb')
-    object = s3.Object('signlanguage-images','index/'+ image[1])
+    object = s3.Object('famouspersons-images2','index/'+ image[1])
     ret = object.put(Body=file,
                     Metadata={'FullName':image[1]})
