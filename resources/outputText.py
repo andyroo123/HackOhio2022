cadcd0d53dcc5888ecb4642883937e32d66d59f3
tates2 <tates.2@osu.edu>
2022-10-09 03:29:12 -0400

    Added text thing

--- resources/outputText.py
+++ resources/outputText.py
@@ -12,13 +12,11 @@ pro = 89
 def killNotePad():
     os.kill(pro.pid, signal.SIGTERM)
 
-def startNotePad():
+def startNotePad(text,end):
     path = r'text.txt'
     global pro
 
     pro = subprocess.Popen(['notepad.exe'])
     time.sleep(3)
-    pyautogui.write('Hello There', interval = 0.1)
-    pyautogui.press('enter')
-    pyautogui.write('How is the Weather?', interval = 0.1)
-    pyautogui.press('enter')
+    while(end == False):
+        pyautogui.write(text, interval = 0.1)
